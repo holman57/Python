313bcd37f1a34ce763d47c734b4f96a5efa0ddef
Luke Holman <holman57@gmail.com>
2024-05-21 21:35:49 -0500

    Update subprocess.py

--- subprocess.py
+++ subprocess.py
@@ -1,15 +1,24 @@
 # https://docs.python.org/3/library/subprocess.html
 import subprocess as proc
+from datetime import datetime
 import random
 import time
 
 
-queue = []
+jobs = [
+    {"id": 0, "priority": 1, "call": "prog.exe", "args": ["arg1", "arg2"], "state": "wait"},
+    {"id": 1, "priority": 2, "call": "prog2.exe", "args": ["arg1"], "state": "wait"},
+    {"id": 2, "priority": 1, "call": "prog.exe", "args": ["arg1"], "state": "wait"}
+]
 
 while True:
-    arg1 = "description"
-    proc.call(("prog.exe", f"{arg1}"))
+    if len(jobs) > 0:
+        j = jobs.pop(0)
+        jid, jp, jcall, jargs, jstate = j["id"], j["priority"], j["call"], j["args"], j["state"]
+        proc.call((jcall, f"{jargs[0]}"))
     buffer = random.randint(1, 5)
     time.sleep(buffer)
-
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+    print("date and time =", dt_string)
 
